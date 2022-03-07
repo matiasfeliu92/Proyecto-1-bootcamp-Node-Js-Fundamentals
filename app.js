73e053556e98da8336e13677368d8ef5ed9993d3
matiasfeliu92 <matumazparrote@gmail.com>
2022-03-07 14:00:17 -0300

    deploy to heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cors = require("cors")
 const morgan = require('morgan')
+// require('dotenv').config()
 
 const app = express()
 
--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
+const config = require('./index')
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'musica'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWRD,
+    database: process.env.DB_DATABASE 
 })
 
 conn.connect((err)=>{
