1b793726e02c2d33403fda5d4d9abd4ac7a18ba0
matiasfeliu92 <matumazparrote@gmail.com>
2022-04-13 13:51:52 -0300

    ajustes de los controladores para eliminar y actualizar canciones

--- controllers/control.js
+++ controllers/control.js
@@ -3,28 +3,22 @@ const {Cancion} = require('../database/db')
 const control = {
 
     create: (req, res) => {
-        Cancion.findOne()
+        Cancion.create({
+            titulo: req.body.titulo,
+            autor: req.body.autor,
+            album: req.body.album,
+            duracion: req.body.duracion,
+            genero: req.body.genero,
+            imagen: req.body.imagen,
+            linkyoutube: req.body.linkyoutube
+        })
         .then((cancion) => {
-            if(cancion) {
-                return res.json({message: "la cancion ya esta en la lista"})
-            } else {
-                Cancion.create({
-                    titulo: req.body.titulo,
-                    autor: req.body.autor,
-                    album: req.body.album,
-                    duracion: req.body.duracion,
-                    genero: req.body.genero,
-                    imagen: req.body.imagen,
-                    linkyoutube: req.body.linkyoutube
-                })
-                .then((cancion) => {
-                    console.log(cancion)
-                    res.status(200).json(cancion)
-                })
-                .catch((err) => {
-                    console.log("No se pudo crear la cancion")
-                })
-            }
+            //console.log(cancion)
+            res.status(200).json(cancion)
+        })
+        .catch((err) => {
+            console.log(err.parent.sqlMessage)
+            res.status(403).json(err.parent.sqlMessage)
         })
     },
 
@@ -32,7 +26,7 @@ const control = {
         Cancion.findAll()
         .then((cancion) => {
             if(cancion) {
-                console.log(cancion)
+                //console.log(cancion)
                 return res.status(200).json(cancion)
             } else {
                 res.status(403).json({message: "no se encontro ninguna cancion en la lista"})
@@ -48,7 +42,7 @@ const control = {
         Cancion.findOne({where:{id: id}})
         .then((cancion) => {
             if(cancion) {
-                console.log(cancion)
+                //console.log(cancion)
                 return res.status(200).json(cancion)
             } else {
                 res.status(403).json({message: "no se encontro ninguna cancion con el id seleccionado"})
@@ -56,6 +50,54 @@ const control = {
         })
         .catch((err) => {
             console.log(`Se ha producido el siguiente error: ${err}`)
+        })
+    },
+
+    update: (req, res) => {
+        const id = req.params.id
+        Cancion.findOne({where:{id: id}})
+        .then((cancion) => {
+            if(cancion) {
+                Cancion.update({
+                    titulo: req.body.titulo,
+                    autor: req.body.autor,
+                    album: req.body.album,
+                    duracion: req.body.duracion,
+                    genero: req.body.genero,
+                    imagen: req.body.imagen,
+                    linkyoutube: req.body.linkyoutube
+                })
+                .then((cancion) => {
+                    console.log(cancion)
+                    res.status(200).json(cancion)
+                })
+                .catch((err) => {
+                    console.log(`Se ha producido el siguiente error: ${err}`)
+                })
+            } else {
+                return res.status(404).json({message: 'No se pudo actualizar la cancion'})
+            }
+        })
+    },
+
+    delete: (req, res) => {
+        const id = req.params.id
+        Cancion.findOne({where:{id: id}})
+        .then((cancion) => {
+            if(cancion) {
+                Cancion.destroy({
+                    where:{id: id}
+                })
+                .then((cancion) => {
+                    //console.log(cancion)
+                    res.status(200).json({message: `se elimino la cancion seleccionada`})
+                })
+            } else {
+                return res.status(403).json({message: 'No se pudo eliminar la cancion'})
+            }
+        })
+        .catch(function(error){
+            console.log(`Se ha producido el siguiente error: `, error);
         })
     }
 
--- routers/router.js
+++ routers/router.js
@@ -3,11 +3,10 @@ const router = express.Router()
 
 const control = require('../controllers/control')
 
-// router.get('/', control.formCreate)
 router.get('/canciones', control.showAll)
-router.post('/guardar', control.create)
 router.get('/detalleCancion/:id', control.showById)
-// router.post('/actualizar/:id', control.update)
-// router.get('/eliminar/:id', control.delete)
+router.post('/guardar', control.create)
+router.put('/actualizar/:id', control.update)
+router.get('/eliminar/:id', control.delete)
 
 module.exports = router
