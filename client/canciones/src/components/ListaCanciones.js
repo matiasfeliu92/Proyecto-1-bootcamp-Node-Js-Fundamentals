7722208ffa9517e3878db062161febe4f471c2c4
matiasfeliu92 <matumazparrote@gmail.com>
2022-04-18 18:13:15 -0300

    ajustes en componente ListaCanciones

--- client/canciones/src/components/ListaCanciones.js
+++ client/canciones/src/components/ListaCanciones.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import './listaCanciones.css'
 import axios from 'axios'
+import './listaCanciones.css';
 
 class ListaCanciones extends Component {
 
@@ -18,20 +18,32 @@ class ListaCanciones extends Component {
     render() {
         return (
             <div className='container'>
-                <h1>MUSIIIIIC PLEASEEEEE</h1>
-                <div className='content'>
-                    {
-                        this.state.canciones.map(cancion => 
-                            <div className='cancionCard'>
-                                <h2>{cancion.titulo}</h2>
-                                <h3>{cancion.autor}</h3>
-                                <h3>{cancion.album}</h3>
-                                <h3>{cancion.genero}</h3>
-                                <p>{cancion.duracion}</p>
-                                <img className='imagenes img-fluid' src={cancion.imagen} alt= 'imagen 1'/>
-                            </div>
-                        )
-                    }
+                <div className='row'>
+                    <h1 className='text-center'>Escucha las canciones que mas te gustan</h1>
+                    
+                        {
+                            this.state.canciones.map(cancion => 
+                                <div className='content col-md-12 d-flex flex-row mt-2 mb-2 justify-content-evenly align-items-md-center'>
+                                    <div className='cancionCard col-md-3'>
+                                        <img className='imagenes img-fluid' src={cancion.imagen} alt= 'imagen 1'/>
+                                    </div>
+                                    <div className='col-md-4'>
+                                        <h2>{cancion.titulo}</h2>
+                                        <h3>{cancion.autor}</h3>
+                                        <h3>{cancion.album}</h3>
+                                        <h3>{cancion.genero}</h3>
+                                        <p>{cancion.duracion}</p>
+                                    </div>
+                                    <div className='col-md-2'>
+                                        <a className='btn btn-primary' href= {cancion.linkyoutube}>Link Youtube</a>
+                                    </div>
+                                    <div className='col-md-3'>
+                                        <a className='btn btn-primary' href= {cancion.linkyoutube}>Actualizar</a>
+                                        <a className='btn btn-primary' href= {cancion.linkyoutube}>Eliminar</a>
+                                    </div>
+                                </div>
+                            )
+                        }
                 </div>
             </div>
         )
