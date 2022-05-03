65af0753b9802aa149c3a756c515ab9406b891f4
matiasfeliu92 <matumazparrote@gmail.com>
2022-05-03 17:25:52 -0300

    agregue createdAt y updatedAt a entity Cancion

--- models/Cancion.ts
+++ models/Cancion.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class Canciones {
@@ -25,4 +25,10 @@ export class Canciones {
 
  @Column({length: 1000})
  linkyoutube: string
+
+ @CreateDateColumn()
+ createdAt: Date
+
+ @UpdateDateColumn()
+ updatedAt: Date
 }
